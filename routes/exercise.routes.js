dd9629e99b47f6201f6968743dd485437d26d695
Vale Quiroga <valentinqramos@outlook.com>
2022-07-08 21:02:33 -0500

    adding populate

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -1,33 +1,40 @@
 const router = require("express").Router()
 const ExerciseModel= require("../models/Exercises.model")
 const fileUploader = require ("../config/cloudinary.config")
+const Snacks = require('../models/Snacks.model')
 
 
-
-router.get("/exercise-list",(req,res,next)=>{
-    
-    ExerciseModel.find()
-    .then(exercise=>{
-        res.render("exercise/exercises",{exercise,userInSession: req.session.currentUser})
-    })
-})
-
+// create
 router.get("/create-exercise",(req,res,next)=>{
-    res.render("exercise/new-exercise",{userInSession: req.session.currentUser})
+    // res.render("exercise/new-exercise",{userInSession: req.session.currentUser})
+    Snacks.find()
+        .then(snackInfo => res.render('exercise/new-exercise',{snackInfo, userInSession: req.session.currentUser}))
+        .catch(error => next(error))
 })
 
+// create
 router.post("/create-exercise",fileUploader.single("exercise-image"),(req,res,next)=>{
 
-const {exercise,series,repetitions,breaks}= req.body
-
-
-ExerciseModel.create({exercise,series,repetitions,breaks,imageUrl:req.file.path})
+const {exercise,series,repetitions,breaks,snacks}= req.body
+ExerciseModel.create({exercise,series,repetitions,breaks,snacks,imageUrl:req.file.path})
 .then(()=> res.redirect("exercise-list"))
 .catch(error=>next (error))
 
 })
 
+// read
+router.get("/exercise-list",(req,res,next)=>{
+    
+    ExerciseModel.find()
+    // .populate('snacks')
+    .then(exercise=>{
+        res.render("exercise/exercises",{exercise,userInSession: req.session.currentUser})
+    })
+    .catch(error => next(error))
+})
 
+
+// update
 router.get("/exercise-edit/:id",(req,res,next)=>{
     const {id} = req.params
 
@@ -38,6 +45,7 @@ router.get("/exercise-edit/:id",(req,res,next)=>{
     .catch(error =>next(error))
 })
 
+// update
 router.post("/exercise-edit/:id",fileUploader.single("exercise-image"),(req,res,next)=>{
     const {id} = req.params
     const {exercise,series,repetitions,breaks, existingImage}= req.body
@@ -53,7 +61,7 @@ router.post("/exercise-edit/:id",fileUploader.single("exercise-image"),(req,res,
     })
     .catch(error =>next(error))
 })
-
+// delete
 router.get("/exercise-delete/:id",(req,res,next)=>{
     const {id}= req.params
 
